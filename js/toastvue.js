c75c8f9ea0f383d449264b14f49afe5b062b26d2
Mark <m.e.giles@gmail.com>
2022-04-08 00:43:13 -0700

    Update toastvue.js

--- js/toastvue.js
+++ js/toastvue.js
@@ -2,13 +2,14 @@ const { Toast } = bootstrap;
 
 export default {
   template: `
-  <div ref="toast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-              <strong class="me-auto">Bootstrap</strong>
-              <small class="text-muted">just now</small>
+  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+  <div ref="toast" class="toast bg-dark border-info" role="alert" aria-live="assertive" aria-atomic="true" >
+            <div class="toast-header bg-info">
+              <strong class="me-auto text-dark">Bootstrap</strong>
+              <small class="text-white-50">just now</small>
               <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            <div class="toast-body">{{msg}}</div>
+            <div class="toast-body text-white">{{msg}}</div>
         </div>
   `,
   data() {
