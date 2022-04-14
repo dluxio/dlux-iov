5390daafe927987b92eee18ba659b0fc01603026
Mark <m.e.giles@gmail.com>
2022-04-14 09:38:06 -0700

    Update navue.js

--- js/navue.js
+++ js/navue.js
@@ -328,60 +328,56 @@ export default {
 <header class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:rgba(42, 48, 54, 0.8); -webkit-backdrop-filter: blur(10px);
   backdrop-filter: blur(10px);">
   <div class="container-fluid">
-	<a class="navbar-brand" href="/"><img src="/img/dlux-hive-logo-alpha.svg" alt="dlux-logo" width="40" height="40"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button>
-    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
+	  <a class="navbar-brand" href="/"><img src="/img/dlux-hive-logo-alpha.svg" alt="dlux-logo" width="40" height="40"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+
       <ul class="navbar-nav me-auto">
-        <li class="nav-item"> <a class="nav-link" href="/hub/"><i class="fa-solid fa-mountain-sun me-2"></i>HUB</a></li>
-        <li class="nav-item"> <a class="nav-link" href="/nfts/"><i class="fa-solid fa-store me-2"></i>NFTS</a></li>
-        <li class="nav-item"> <a class="nav-link" href="/dex#dlux"><i class="fa-solid fa-building-columns me-2"></i>DEX</a></li>
-        <li class="nav-item"> <a class="nav-link" href="/apps/"><i class="fa-solid fa-globe me-2"></i>APPS</a></li>
-        <li class="nav-item d-none"> <a class="nav-link " href="/docs/">DOCS</a></li>
+        <li class="nav-item"><a class="nav-link" href="/hub/"><i class="fa-solid fa-mountain-sun me-2"></i>HUB</a></li>
+        <li class="nav-item"><a class="nav-link" href="/nfts/"><i class="fa-solid fa-store me-2"></i>NFTS</a></li>
+        <li class="nav-item"><a class="nav-link" href="/dex#dlux"><i class="fa-solid fa-building-columns me-2"></i>DEX</a></li>
+        <li class="nav-item"><a class="nav-link" href="/apps/"><i class="fa-solid fa-globe me-2"></i>APPS</a></li>
+        <li class="nav-item"><a class="nav-link" href="/docs/"><i class="fa-solid fa-book me-2"></i>DOCS</a></li>
       </ul>
-     
 
-  <div v-show="!user">
-	<ul class="navbar-nav d-flex align-items-center me-5" id="loginMenu" >
-	<li class="nav-item"><a class="nav-link acct-link" href="/about/">About</a></li>
-	<li class="nav-item"><a class="nav-link acct-link" href="https://signup.hive.io/">Get Account</a></li>
-  <li class="nav-item"><a class="nav-link acct-link" href="/docs/">Docs</a></li>
-	<li class="nav-item">
-  <div class="input-group input-group-sm">
-    <button class="btn btn-primary ms-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasUsers" aria-controls="offcanvasUsers">Login</button>
-  </div>
-  </li>
+      <div v-show="!user">
+	      <ul class="navbar-nav d-flex align-items-center me-5" id="loginMenu" >
+	        <li class="nav-item"><a class="nav-link acct-link" href="/about/">About</a></li>
+          <li class="nav-item"><a class="nav-link acct-link" href="/docs/">Docs</a></li>
+	        <li class="nav-item"><a class="nav-link acct-link" href="https://signup.hive.io/">Get Account</a></li>
+	        <li class="nav-item">
+            <div class="input-group input-group-sm">
+              <button class="btn btn-primary ms-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasUsers" aria-controls="offcanvasUsers">Login</button>
+            </div>
+          </li>
+      	</ul>
+      </div>
 
-	</ul>
-  </div>
-
-    <div class="me-5" v-show="user" id="userMenu">
-	  <ul class="nav navbar-nav">
-    
-		<li class="nav-item my-auto">
-			<a class="nav-link" href="/new/" data-bs-toggle="tooltip"  title="Create a new app">
-				<i class="fas fa-fw fa-lg fa-plus me-2"></i>NEW</a></li>
-        <li class="nav-item my-auto"><a class="nav-link" href="/docs/"><i class="fa-solid fa-book me-2"></i>DOCS</a></li>
-		<li class="nav-item dropdown">
-		  <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" role="button" aria-expanded="false" data-bs-toggle="dropdown" href="#">
-			  <img :src="avatar" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light me-1 cover">
-			  <span id="userName">{{user}}</span></a>
-          <ul class="dropdown-menu dropdown-menu-dark pt-0" aria-labelledby="userDropdown">
-			 <li><a class="dropdown-item" href="/me#blog/" onClick="showTab('blog')"><i class="fas fa-user fa-fw me-2"></i>Profile</a></li>
-			 <li><a class="dropdown-item" href="/me#wallet/" onClick="showTab('wallet')"><i class="fas fa-wallet fa-fw me-2"></i>Wallet</a></li>
-			 <li><a class="dropdown-item" href="/me#inventory/" onClick="showTab('inventory')"><i class="fas fa-boxes fa-fw me-2"></i>Inventory</a></li>
-			 <li><a class="dropdown-item" href="/me#node/" onClick="showTab('node')"><i class="fas fa-robot fa-fw me-2"></i>Node</a></li>
-			 <li><a class="dropdown-item" href="/me#settings/" onClick="showTab('settings')"><i class="fas fa-cog fa-fw me-2"></i>Settings</a></li>
-             <li><hr class="dropdown-divider"></li>
-			 <li><a class="dropdown-item" href="/about/"><i class="fas fa-info-circle fa-fw me-2"></i>About</a></li>
-             <li><hr class="dropdown-divider"></li>
-             <li><a class="dropdown-item" href="#" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasUsers" aria-controls="offcanvasUsers"><i class="fas fa-user-friends me-2"></i>Switch User</a></li>
-			 <li><a class="dropdown-item" href="#" @click="logout()"><i class="fas fa-power-off fa-fw me-2"></i>Logout</a></li>
-		</ul>
-        </li>
-      </ul>
-	</div>
+      <div class="me-5" v-show="user" id="userMenu">
+	      <ul class="nav navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="/new/"><i class="fa-solid fa-plus me-2"></i>MAKE</a></li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" role="button" aria-expanded="false" data-bs-toggle="dropdown" href="#">
+			      <img :src="avatar" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light me-1 cover">
+			      <span id="userName">{{user}}</span></a>
+            <ul class="dropdown-menu dropdown-menu-dark pt-0" aria-labelledby="userDropdown">
+			        <li><a class="dropdown-item" href="/me#blog/" onClick="showTab('blog')"><i class="fas fa-user fa-fw me-2"></i>Profile</a></li>
+			        <li><a class="dropdown-item" href="/me#wallet/" onClick="showTab('wallet')"><i class="fas fa-wallet fa-fw me-2"></i>Wallet</a></li>
+			        <li><a class="dropdown-item" href="/me#inventory/" onClick="showTab('inventory')"><i class="fas fa-boxes fa-fw me-2"></i>Inventory</a></li>
+			        <li><a class="dropdown-item" href="/me#node/" onClick="showTab('node')"><i class="fas fa-robot fa-fw me-2"></i>Node</a></li>
+			        <li><a class="dropdown-item" href="/me#settings/" onClick="showTab('settings')"><i class="fas fa-cog fa-fw me-2"></i>Settings</a></li>
+              <li><hr class="dropdown-divider"></li>
+			        <li><a class="dropdown-item" href="/about/"><i class="fas fa-info-circle fa-fw me-2"></i>About</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasUsers" aria-controls="offcanvasUsers"><i class="fas fa-user-friends me-2"></i>Switch User</a></li>
+			        <li><a class="dropdown-item" href="#" @click="logout()"><i class="fas fa-power-off fa-fw me-2"></i>Logout</a></li>
+		        </ul>
+          </li>
+        </ul>
+	    </div>
     </div>
-    </div>
+  </div>
 </header>
 <div class="position-fixed bottom-0 end-0 p-3 toast-container" style="z-index: 11">
   <div v-for="op in ops">  
