ac2845b97d5f67bf846073f8203381fdad1ead98
Mark <m.e.giles@gmail.com>
2022-04-21 10:27:25 -0700

    Update navue.js

--- js/navue.js
+++ js/navue.js
@@ -681,7 +681,7 @@ export default {
           <img :src="avatar" id="userImage" alt="" width="50" height="50" class="img-fluid rounded-circle bg-light me-2 cover">
           <span id="userName">{{user}}</span>
           <a class="btn btn-outline-success ms-auto" :href="HAS_.uri" v-if="!HAS_.token && HAS"><i class="fas fa-power-off fa-fw me-2"></i>Login</a>
-          <a class="btn btn-outline-danger ms-auto" href="#" @click="logout()" v-if="HAS_.token || HKC"><i class="fas fa-power-off fa-fw me-2"></i>Logout</a>
+          <a class="btn btn-outline-danger ms-auto" href="#/" @click="logout()" v-if="HAS_.token || HKC"><i class="fas fa-power-off fa-fw me-2"></i>Logout</a>
         </div>
       </div>
 
@@ -694,11 +694,11 @@ export default {
         </div>
       </div>
       <div class="d-flex justify-content-between align-items-center m-3 pb-3 border-dark border-bottom" v-if="!filterUsers" v-for="name in recentUsers">
-        <div class="flex-fill text-center"><a class="link-info" href="#" @click="setUser(name);toggleAccountMenu()" data-bs-dismiss="offcanvas">@{{name}}</a></div>
+        <div class="flex-fill text-center"><a class="link-info" href="#/" @click="setUser(name);toggleAccountMenu()">@{{name}}</a></div>
         <div class="flex-shrink"><a href="#" @click="deleteRecentUser(name)" alt="Remove username" class="ms-auto link-secondary"><i class="fa-solid fa-trash-can"></i></a></div>
       </div>
       <div class="d-flex justify-content-between align-items-center m-3 pb-3 border-dark border-bottom" v-if="filterUsers" v-for="name in filterRecents">
-        <div class="flex-fill text-center"><a class="link-info" href="#" @click="setUser(name);toggleAccountMenu()" data-bs-dismiss="offcanvas">@{{name}}</a></div>
+        <div class="flex-fill text-center"><a class="link-info" href="#/" @click="setUser(name);toggleAccountMenu()">@{{name}}</a></div>
         <div class="flex-shrink"><a href="#" @click="deleteRecentUser(name);searchRecents()" alt="Remove username" class="ms-auto link-secondary"><i class="fa-solid fa-trash-can"></i></a></div>
       </div>
     </div>
