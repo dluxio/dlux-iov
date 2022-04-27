a2fee905e9cfad8d3b70057cc111a4bbfc8e66ab
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-27 01:39:27 -0300

    blarg

--- js/cycler.js
+++ js/cycler.js
@@ -5,20 +5,40 @@ export default {
       index: 0,
     };
   },
-  template: `
-  <div>{{text}}</div>
-        `,
+  template: `<div>
+  <transition name="fade" mode="out-in" appear>
+  <p v-show="text" transition="fade">{{ text }}</p>
+  </transition></div>`,
   props: ["items", "interval"],
-  watch: {
-  },
+  watch: {},
   mounted() {
-      this.text = this.items[this.index];
-      this.cycle();
+    this.text = this.items[this.index];
+    this.cycle();
+    var css_text = `
+    .fade-leave-active {
+  transition: opacity .4s ease-in;
+}
+.fade-enter-active {
+  transition: opacity .4s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0.0;
+}
+.fade-leave, .fade-enter-to {
+  opacity: 1.0;
+}
+`;
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.setAttributeNode(document.createAttribute("scopped"));
+    css.appendChild(document.createTextNode(css_text));
+    this.$el.appendChild(css);
   },
   methods: {
     cycle() {
+      this.text = "";
       this.index = (this.index + 1) % this.items.length;
-        this.text = this.items[this.index];
+      this.text = this.items[this.index];
       setTimeout(
         function () {
           this.cycle();
