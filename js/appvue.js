15f78a05fdcfbaf8e61aa746711d94e32e5a25da
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-26 23:29:21 -0300

    blarg

--- js/appvue.js
+++ js/appvue.js
@@ -664,6 +664,27 @@ var app = new Vue({
                 console.log("no author or permlink", a, p)
             }
         },
+        readRep(rep2){
+            function log10(str) {
+              const leadingDigits = parseInt(str.substring(0, 4));
+              const log = Math.log(leadingDigits) / Math.LN10 + 0.00000001;
+              const n = str.length - 1;
+              return n + (log - parseInt(log));
+            }
+              if (rep2 == null) return rep2;
+              let rep = String(rep2);
+              const neg = rep.charAt(0) === "-";
+              rep = neg ? rep.substring(1) : rep;
+
+              let out = log10(rep);
+              if (isNaN(out)) out = 0;
+              out = Math.max(out - 9, 0); // @ -9, $0.50 earned is approx magnitude 1
+              out = (neg ? -1 : 1) * out;
+              out = out * 9 + 25; // 9 points per magnitude. center at 25
+              // base-line 0 to darken and < 0 to auto hide (grep rephide)
+              out = parseInt(out);
+              return out;
+        },
         getQuotes() {
             fetch(
                 "https://api.coingecko.com/api/v3/simple/price?ids=hive&amp;vs_currencies=usd"
