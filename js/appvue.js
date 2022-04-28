74ff4241be3e61d58f49abfaee2182cb0d75bd4a
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-28 01:44:58 -0300

    stuff

--- js/appvue.js
+++ js/appvue.js
@@ -776,7 +776,7 @@ var app = new Vue({
               this.posturls[res.result.url].type = type;
               this.posturls[res.result.url].preview = this.removeMD(
                 this.posturls[res.result.url].body
-              ).substr(0, 70);
+              ).substr(0, 250);
               this.posturls[res.result.url].ago = this.timeSince(
                 this.posturls[res.result.url].created
               );
