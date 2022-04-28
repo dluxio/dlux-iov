1cae92b9d06f132f3581011d393bd5cb68fccb45
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-28 01:27:02 -0300

    stuff

--- js/appvue.js
+++ js/appvue.js
@@ -792,13 +792,9 @@ var app = new Vue({
     },
     timeSince(date) {
       var seconds = Math.floor((new Date() - new Date(date)) / 1000);
-      var interval = Math.floor(seconds / 31536000);
-      if (interval > 1) {
-        return interval + " years";
-      }
-      interval = Math.floor(seconds / 2592000);
-      if (interval > 1) {
-        return interval + " months";
+      var interval = Math.floor(seconds / 86400)
+      if (interval > 7) {
+        return new Date(date).toLocaleDateString();
       }
       interval = Math.floor(seconds / 86400);
       if (interval > 1) {
