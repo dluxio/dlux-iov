e414573045f9af9390dd59483806e21998b90d84
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-29 18:31:51 -0300

    app viewer

--- js/marker.js
+++ js/marker.js
@@ -1,15 +1,17 @@
 export default {
   data() {
     return {
+      text: '',
     };
   },
   template: `<div>
+  <textarea v-show="!md" v-model="text" class="form-control" rows="3"></textarea>
   <div v-html="compiledMarkdown"></div>
 </div>`,
   props: ["md", "author", "permlink" ],
   computed: {
     compiledMarkdown: function () {
-      return marked.parse(this.md.replace(`[View in VR @ dlux.io](https://dlux.io/dlux/@${this.author}/${this.permlink})`, ''), { sanitize: true });
+      return marked.parse(this.md ? this.md.replace(`[View in VR @ dlux.io](https://dlux.io/dlux/@${this.author}/${this.permlink})`, '') : this.text, { sanitize: true });
     },
   },
   // methods: {
