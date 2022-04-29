996e63e07aa33d439715c935cc89b9518ce4fb55
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-29 05:06:04 -0300

    app viewer

--- js/appvue.js
+++ js/appvue.js
@@ -366,6 +366,7 @@ var app = new Vue({
           id: "",
           slider: 100,
           title: "",
+          type: "360"
           url: "",
           children: [],
           total_payout_value: 0,
