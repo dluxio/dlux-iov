1040a95a68aa429d993aa6ab1697c80d06fde656
Steven Ettinger <sjettingerjr@gmail.com>
2022-06-10 12:39:20 -0300

    me page

--- js/vueme.js
+++ js/vueme.js
@@ -666,6 +666,7 @@ var app = new Vue({
         })
         .then((re) => {
             var rez = re.result[0]
+            console.log(rez, rez.posting_json_metadata);
             rez.posting_json_metadata = JSON.parse(rez.posting_json_metadata);
           if (re.result.length) this[key] = re.result[0]
           else this[key] = false;
