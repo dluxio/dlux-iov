d2ab2b04454342a24c1842e541923d27da93b3f3
Steven Ettinger <sjettingerjr@gmail.com>
2022-10-20 19:58:16 -0300

    ipfs port

--- js/nftsvue.js
+++ js/nftsvue.js
@@ -598,7 +598,7 @@ if(window.addEventListener){window.addEventListener("message",onMessage,false);}
         if (window.IpfsHttpClient) {
           const ipfs = window.IpfsHttpClient({
             host: "ipfs.dlux.io",
-            port: "5002",
+            port: "443",
             protocol: "https",
             headers: {
               account: this.account,
