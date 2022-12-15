c5d8b49c7efdc7a83e76dabc2344301faf70b8f4
disregardfiat <sjettingerjr@gmail.com>
2022-12-15 17:09:49 -0300

    test upload

--- js/vueme.js
+++ js/vueme.js
@@ -721,7 +721,7 @@ var app = new Vue({
               ) {
                 Hash.of(fileContent).then((hash) => {
                   this.File[i].md5 = hash;
-                  this.File[i].blob = new Blob([fileContent])
+                  this.File[i].blob = new File([fileContent], event.currentTarget.File.name)
                   const file = this.File[i];
                   this.File.splice(i, 1, file);
                 });
