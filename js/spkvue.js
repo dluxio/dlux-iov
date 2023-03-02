8563ded735328bfcb107e345c8efa5248609b327
disregardfiat <sjettingerjr@gmail.com>
2023-03-02 01:09:38 -0300

    minor

--- js/spkvue.js
+++ js/spkvue.js
@@ -1040,7 +1040,8 @@ var app = new Vue({
     signNUpload() {
       console.log(this.contract.id)
       var header = this.account
-      header += ':'
+      header += '|'
+      header += this.account
       header += this.contract.id.toString()
       var body = ""
       var names = Object.keys(this.FileInfo)
