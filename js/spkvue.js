f534e57a1a23935d35b094dba1f3c4d9be31c74e
disregardfiat <sjettingerjr@gmail.com>
2023-03-02 16:37:16 -0300

    minor

--- js/spkvue.js
+++ js/spkvue.js
@@ -1108,7 +1108,7 @@ var app = new Vue({
           // updateFileElement(fileObj);
         },
         onProgress(e,f) {
-          console.log(e, f, f.name)
+          console.log(e, f, this.FileInfo, this.File)
           this.File[this.FileInfo[f.name].index].actions.pause = true
           this.File[this.FileInfo[f.name].index].actions.resume = false
           this.File[this.FileInfo[f.name].index].actions.cancel = true
