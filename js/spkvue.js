4ae5663fd25a043d3bf7504b270cf37d9a2e739c
disregardfiat <sjettingerjr@gmail.com>
2023-03-02 15:56:51 -0300

    minor

--- js/spkvue.js
+++ js/spkvue.js
@@ -847,6 +847,7 @@ var app = new Vue({
       }
     },
     dragFile(e) {
+      console.log(e)
       for (var i = 0; i < e.dataTransfer.files.length; i++) {
         var reader = new FileReader();
         reader.File = e.dataTransfer.files[i]
