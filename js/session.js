7f4c1c4ab3436972043d7f5763243d28ea1fb2fb
Mark <m.e.giles@gmail.com>
2023-04-20 19:49:01 -0700

    Update session.js

--- js/session.js
+++ js/session.js
@@ -1120,6 +1120,10 @@ function reqsign(op, req) { //requests keychain to sign and broadcast
     })
 }
 
+function showTab(str) {
+    $('#usertabs a[href="#'+str+'"]').tab("show");
+}
+
 // function voteProp(props, user) {
 //     reqsign([
 //             "update_proposal_votes",
