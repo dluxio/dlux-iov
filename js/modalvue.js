ea77b47f87ee922056c6e02d35ef6159f196f684
disregardfiat <sjettingerjr@gmail.com>
2023-04-26 03:44:38 -0400

    ++

--- js/modalvue.js
+++ js/modalvue.js
@@ -108,11 +108,12 @@ export default {
                       <div v-if="func == 'Election'">
                       <div v-if="valWorkable.length">
                       <h3 class="mb-3">Chosen Validators</h3>
+                      <h5 class="m-0"> Node </h5> | <h5 class="m-0">Vote Weight</h5>
                         <ul class="mx-5 p-0">
                           <div v-for="(node, index) in valWorkable">
                             <li @dragstart="pick($event, node, index)" @dragover.prevent @dragenter.prevent @drop="move($event, node, index)" class="border border-secondary rounded d-flex align-items-center justify-content-between p-2 my-2 drop-zone" draggable="true" style="cursor: move;">
                               <i class="fa-solid fa-grip-lines"></i>  
-                              <h5 class="m-0">@{{node.self}}</h5>|<h5>{{formatNumber(((30 - index )/ 30)* 100, 1,  '.', ',')}}%</h5>
+                              <h5 class="m-0">@{{node.self}}  |  {{formatNumber(((30 - index )/ 30)* 100, 1,  '.', ',')}}%</h5>
                               <button class="btn btn-primary" @click="sub(node)" type="button"><i class="fa-solid fa-minus"></i></button>
                             </li>
                           </div>
