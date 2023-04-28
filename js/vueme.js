91a5ab0414364c934f990d4f56f2864671e1c848
disregardfiat <sjettingerjr@gmail.com>
2023-04-28 03:37:06 -0400

    ++

--- js/vueme.js
+++ js/vueme.js
@@ -629,6 +629,7 @@ var app = new Vue({
       },
       posturls: {},
       new: [],
+      theirs: [],
       trending: [],
       promoted: [],
       search: [],
@@ -636,7 +637,7 @@ var app = new Vue({
         sort: "time",
         searchTerm: "",
         bitMask: 0,
-        entry: "new",
+        entry: "theirs",
         search: {
           a: 10,
           o: 0,
@@ -656,6 +657,12 @@ var app = new Vue({
           p: false,
         },
         promoted: {
+          a: 10,
+          o: 0,
+          e: false,
+          p: false,
+        },
+        theirs: {
           a: 10,
           o: 0,
           e: false,
@@ -2374,9 +2381,8 @@ function bidNFT(setname, uid, bid_amount, type, callback){
       }
     },
     selectPosts(modal, reset) {
-      var arr = !reset ? this.displayPosts : []
+      var arr = [];
       for (var i = 0; i < this[this.postSelect.entry].length; i++) {
-        console.log(modal, this.posturls[this[this.postSelect.entry][i]])
         if (this.posturls[this[this.postSelect.entry][i]])
           arr.push(this.posturls[this[this.postSelect.entry][i]]);
       }
