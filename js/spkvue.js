3fd80c6fa5fca0f216847ec1a4c5bb7e3ba4ced9
disregardfiat <sjettingerjr@gmail.com>
2023-04-27 23:01:02 -0400

    ++

--- js/spkvue.js
+++ js/spkvue.js
@@ -943,7 +943,7 @@ var app = new Vue({
           //   ) {
           Hash.of(buffer.Buffer(fileContent)).then(hash => {
             console.log('hereasdasd')
-            const dict = { hash, index: this.File.length, size: event.currentTarget.File.size, name: event.currentTarget.File.name, status: 'Pending Signature' }
+            const dict = { fileContent, hash, index: this.File.length, size: event.currentTarget.File.size, name: event.currentTarget.File.name, status: 'Pending Signature' }
             this.FileInfo[dict.name] = dict
             // var File = e.dataTransfer.files[i];
             var File = event.currentTarget.File
