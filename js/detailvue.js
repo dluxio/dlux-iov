7154dd10902b54821a478cdc60396756f779c9e2
disregardfiat <sjettingerjr@gmail.com>
2023-05-06 20:05:24 -0400

    md

--- js/detailvue.js
+++ js/detailvue.js
@@ -14,7 +14,7 @@ export default {
         "pop-vue": Pop,
         "replies": Replies,
     },
-    template: `<div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-hidden="true" @blur="goBack()">
+    template: `<div class="modal fade" :class="{'show': !hide}" id="detailModal" tabindex="-1" role="dialog" aria-hidden="true" @blur="goBack()">
     <div class="modal-dialog modal-full modal-xl modal-dialog-centered" style="max-width: 1000px;"
         role="document">
         <div class="modal-content bg-img-none text-white">
