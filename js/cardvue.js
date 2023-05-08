0c667bf453b5da2a0dfb660dbfe2efeaea43a7b5
disregardfiat <sjettingerjr@gmail.com>
2023-05-08 16:50:48 -0400

    ++

--- js/cardvue.js
+++ js/cardvue.js
@@ -114,7 +114,7 @@ export default {
                                     <button type="button" class="btn btn-sm btn-primary me-1" :disabled="extendcost[name] > broca_calc(broca)" @click="extend(contracts[name], extendcost[name])"><i class="fa-solid fa-clock-rotate-left fa-fw me-1"></i>Extend</button>
                                     <button type="button" v-if="contracts[name].t == account" class="btn btn-sm btn-warning me-1" @click="cancel_contract(contracts[name])"><i class="fa-solid fa-clock-rotate-left fa-fw me-1"></i>Cancel</button>
                                     <input :id="'spread-' + name" type="checkbox" v-model="spread" @change="updateCost(name)">
-                                          <label :for="'spread-' + name"><i class="fa-solid fa-tower-broadcast"></i></label>
+                                          <label :for="'spread-' + name"><i class="fa-solid fa-tower-broadcast"></i>Amplify </label>
                                     <button type="button" class="btn btn-sm btn-secondary me-1" data-bs-toggle="collapse"
                                         :data-bs-target="'#contract-' + post.author + '-' + post.permlink"><span><i class="fa-solid fa-xmark fa-fw"></i></span></button>
                                     <div class="ms-auto text-primary">{{formatNumber(extendcost[name], 0, '.',',')}}
