f1db9431eddec27df7c3e87642f6130ba6c5ccb9
disregardfiat <sjettingerjr@gmail.com>
2023-05-13 17:37:15 -0400

    ++

--- js/dexvue.js
+++ js/dexvue.js
@@ -65,7 +65,7 @@ var app = new Vue({
         height: 400,
         toolbar: true,
         overlays: false,
-        bg: `#112222`,
+        bg: `#113`,
       },
       barcount: 500,
       barwidth: 3600000 * 6,
