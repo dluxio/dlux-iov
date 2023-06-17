051ecaaea4a47cc64c499489ede3c4c5984657b2
Mark <m.e.giles@gmail.com>
2023-06-17 15:21:34 -0700

    Update nftcard.js

--- js/nftcard.js
+++ js/nftcard.js
@@ -55,9 +55,6 @@ export default {
         v-html="item.HTML">
       </div>
     </a>
-    <button type="button" class="btn btn-primary" @click="modalIndex('details')" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Launch demo modal
-                </button>
   </div>
   <!-- MINT BODY -->
   <div class="p-2 flex-grow-1 d-flex" v-if="mint">
