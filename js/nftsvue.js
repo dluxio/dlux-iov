44c1d51d391563aaa3d4be0ee08ed718d331a3df
disregardfiat <sjettingerjr@gmail.com>
2023-06-21 21:54:18 -0400

    fix

--- js/nftsvue.js
+++ js/nftsvue.js
@@ -651,16 +651,19 @@ if(window.addEventListener){window.addEventListener("message",onMessage,false);}
           // splice
           this.NFTselect.keys.splice(i, 1)
           i--
+          continue
         }
         if((!term || term ==  this.NFTselect.keys[i]) && this.NFTselect.keys[i].indexOf('Set') == 0){
           this.chains[this.NFTselect.keys[i].split(':')[2]].sets[this.NFTselect.keys[i].split(':')[1]].enabled = false
           this.NFTselect.keys.splice(i, 1)
           i--
+          continue
         }
         if((!term || term ==  this.NFTselect.keys[i]) && this.NFTselect.keys[i].indexOf('Status') == 0){
           this.selectors[this.NFTselect.keys[i].split(':')[1]].enabled = false
           this.NFTselect.keys.splice(i, 1)
           i--
+          continue
         }
       }
       if(!term)this.NFTselect.keys = []
