576d7648ceef5f4e00b5fa8ad02972e29eda7721
disregardfiat <sjettingerjr@gmail.com>
2023-07-18 18:48:50 -0400

    ++

--- js/fttransfer.js
+++ js/fttransfer.js
@@ -89,7 +89,7 @@ export default {
                                  class="fas fa-caret-square-left"></i></a>
                         </h2>
                         <small class="ms-auto text-muted"><i>
-                                {{numitems}} sets</i></small>
+                                {{numitems}} set{{numitems > 1 ? 's' : ''}}</i></small>
                         <h2 class="ms-auto"><a class="text-muted p-3" role="button" @click="modalNext()"><i
                                  class="fas fa-caret-square-right"></i></a>
                         </h2>
