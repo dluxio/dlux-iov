ddd40d4f255176af388e9afaf4f6d8591f5b6333
Mark <m.e.giles@gmail.com>
2023-10-27 12:40:24 -0700

    Update vueme.js

--- js/vueme.js
+++ js/vueme.js
@@ -9,7 +9,7 @@ import Ratings from "/js/ratings.js";
 import MDE from "/js/mde.js";
 import Replies from "/js/replies.js";
 import CardVue from "./cardvue.js";
-import ContractsVue from "/js/contractsvue.js";
+import ContractVue from "/js/contractsvue.js";
 import FilesVue from "/js/filesvue.js";
 import ExtensionVue from "/js/extensionvue.js";
 import DetailVue from "./detailvue.js";
