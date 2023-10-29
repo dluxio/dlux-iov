cd6ce965910bac98c94e9b42aad37957f5bc9373
Mark <m.e.giles@gmail.com>
2023-10-29 12:38:47 -0700

    Update filesvue.js

--- js/filesvue.js
+++ js/filesvue.js
@@ -7,14 +7,17 @@ export default {
     <div v-if="hasFiles" style="background-color: #16191C;" class="p-2">
         <div class="bg-darkg rounded my-2 p-2">
             <div>
-                <div class="d-flex flex-wrap">
-                    <div v-for="(size, file) in files" class="d-flex flex-column p-2 rounded m-1"
+                <div class="card-group d-flex flex-wrap">
+                    <div v-for="(size, file) in files" class="card d-flex flex-column p-2 rounded m-1"
                         style="background-color: #000000;">
-                        <a :href="'https://ipfs.io/ipfs/' + file" target="_blank" class="no-decoration"><img
-                                :src="'https://ipfs.io/ipfs/' + file" onerror="this.style.display='none'"
-                                class="img-fluid" style="max-width: 300px;" :alt="file"><br><span class="small text-muted">{{file}}</span></a>
-                        <button type="button" class="btn btn-primary mt-2" 
-                            @click="addAsset(file, contract.i)"><i class="fa-solid fa-square-plus me-2"></i>Add</button>
+                        <a :href="'https://ipfs.io/ipfs/' + file" target="_blank" class="d-flex card-body flex-wrap no-decoration" style="max-width: 300px;">
+                            <img :src="'https://ipfs.io/ipfs/' + file" onerror="this.style.display='none'"
+                                class="img-fluid" :alt="file">
+                            <span class="text-break small text-muted">{{file}}</span></a>
+                       <div class="card-footer">
+                            <button type="button" class="btn btn-primary mt-2" 
+                            @click="addAsset(file, contract.i)"><i class="fa-solid fa-magnifying-glass me-2"></i>Preview</button>
+                            </div>
                     </div>
                 </div>
             </div>
