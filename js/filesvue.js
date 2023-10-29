46d64e86cc5064df81c78d02ca0c4ceb87a1f53d
Mark <m.e.giles@gmail.com>
2023-10-29 12:46:33 -0700

    Update filesvue.js

--- js/filesvue.js
+++ js/filesvue.js
@@ -4,24 +4,26 @@ export default {
     <div v-if="!hasFiles" class="p-3">
         <p class="m-0 text-center">Looks like there's nothing here yet.</p>
     </div>
-    <div v-if="hasFiles" style="background-color: #16191C;" class="p-2">
-        <div class="bg-darkg rounded my-2 p-2">
+    <div v-if="hasFiles">
+       
             <div>
-                <div class="card-group d-flex flex-wrap">
-                    <div v-for="(size, file) in files" class="card d-flex flex-column p-2 rounded m-1"
-                        style="background-color: #000000;">
-                        <a :href="'https://ipfs.io/ipfs/' + file" target="_blank" class="d-flex card-body flex-wrap no-decoration" style="max-width: 300px;">
+                <div class="card-group">
+                    <div v-for="(size, file) in files" class="card p-0" style="max-width: 300px;">
+                        <a :href="'https://ipfs.io/ipfs/' + file" target="_blank" class="d-flex flex-wrap no-decoration" >
                             <img :src="'https://ipfs.io/ipfs/' + file" onerror="this.style.display='none'"
-                                class="img-fluid" :alt="file">
-                            <span class="text-break small text-muted">{{file}}</span></a>
-                       <div class="card-footer">
-                            <button type="button" class="btn btn-primary mt-2" 
+                                class="card-img-top" :alt="file">
+                                <div class="card-body">
+                                    <span class="text-break small text-muted">{{file}}</span>
+                                </div>
+                            </a>
+                       <div class="card-footer text-center">
+                            <button type="button" class="btn btn-primary" 
                             @click="addAsset(file, contract.i)"><i class="fa-solid fa-magnifying-glass me-2"></i>Preview</button>
                             </div>
                     </div>
                 </div>
             </div>
-        </div>
+        
     </div>
 </div>
    `,
