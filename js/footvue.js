430417910ae10688a6d0f354f1f96159fcd106c4
Mark <m.e.giles@gmail.com>
2024-01-17 09:11:04 -0800

    Update footvue.js

--- js/footvue.js
+++ js/footvue.js
@@ -3,18 +3,18 @@ export default {
   <footer class="footer bg-dark pt-5">
   <!-- Footer Elements -->
   <div class="container text-center">
-        <div class="mb-5">
+        <div class="mb-4">
           <!-- Github -->
           <a href="https://github.com/dluxio" target="_blank">
-            <i class="fab fa-github fa-lg mx-md-3 mx-2 fa-2x"></i>
+            <i class="fab fa-github fa-2x mx-md-3 mx-2"></i>
           </a>
           <!-- Discord -->
           <a href="https://discord.gg/Beeb38j" target="_blank">
-            <i class="fab fa-discord fa-lg mx-md-3 mx-2 fa-2x"></i>
+            <i class="fab fa-discord fa-2x mx-md-3 mx-2"></i>
           </a>
           <!-- Hive -->
           <a href="https://peakd.com/@dlux-io" target="_blank">
-          <i class="fab fa-hive mx-md-3 mx-2 fa-2x"></i>
+          <i class="fab fa-hive fa-2x mx-md-3 mx-2"></i>
           </a>
           <!--Twitter -->
           <a href="https://twitter.com/dluxxr" target="_blank">
@@ -24,7 +24,7 @@ export default {
   </div>
   <!-- Footer Elements -->
   <!-- Copyright -->
-  <div class="footer-copyright text-center text-white-50 pb-2">Copyright © 2024 dlux.io</div>
+  <div class="footer-copyright text-center text-white-50 pb-3">Copyright © 2024 dlux.io</div>
   </footer>
   `,
 };
