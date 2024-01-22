86715f1c524cd5f28219f39b7f4e6798f876415a
disregardfiat <sjettingerjr@gmail.com>
2024-01-22 00:26:54 -0300

    ++

--- js/bennies.js
+++ js/bennies.js
@@ -1,60 +1,57 @@
 export default {
-    template: `
-    <div>
-        <p>Current Beneficiaries: ({{bennies?.length}}/8) {{total/100}}%</p><button v-if="!dlux" class="btn btn-sm btn-warning" @click="addBenny('dlux-io', 1000)">Include in DLUX Ecosystem</button>
-        <div class="table-responsive">
-        <table class="table align-middle">
-            <thead>
-                <tr>
-                    <th class="w-50">Username</th>
-                    <th class="text-center">Reward</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr v-for="ben in bennies">
-                    <td class="ps-0 w-50">@{{ben.account}}</td>
-                    <td class="text-center">
-                        <span class="pe-05"><button type="button" class="btn btn-sm btn-secondary" @click="decBen(ben)">-</button></span>
-                        <span>{{ben.weight/100}}%</span>
-                        <span class="ps-05"><button type="button" class="btn btn-sm btn-secondary" @click="incBen(ben)">+</button></span>
-                    </td>
-                    <td class="pe-0 text-end"><button class="btn btn-danger" @click="subBenny(ben.account)"><i class="fa-solid fa-trash-can fa-fw"></i></button></td>
-                </tr>
-                <tr v-if="bennies.length < 8 && total < 10000" style="border-bottom-style: hidden !important;">
-                    <td class="ps-0 w-50">
-                    <div class="position-relative">
-                    <span class="position-absolute start-0 top-50 translate-middle-y ps-2">
-                       <i class="fa-solid fa-at fa-fw"></i>
-                    </span>
-                            <input type="text" placeholder="username" class="ps-4 pe-5 text-info form-control" v-model="addAccount">
-                            <button class="position-absolute end-0 top-50 translate-middle-y btn btn-outline-secondary border-0 dropdown-toggle square rounded-end" :disabled="!favorites.length" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-star me-1 fa-fw"></i></button>
-                            <ul class="dropdown-menu bg-black dropdown-menu-dark">
-                                <li v-for="acc in favorites" class="d-flex align-items-center justify-content-between">
-                                    <a class="dropdown-item rounded-end" role="button" @click="addAccount = acc">@{{acc}}</a>
-                                    <a @click="removeFavorite(acc)" class="mx-1 btn btn-sm btn-secondary" role="button">
-                                        <i class="fa-solid fa-xmark fa-fw"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </td>
-                    <td class="text-center">
-                    <div class="position-relative">
-                            <input type="number" step="0.01" min="0.01" :max="100 - (total/100)" placeholder="amount" class="pe-4 form-control text-center" v-model="addWeight">
-                            <span class="position-absolute end-0 top-50 translate-middle-y pe-2">
-                            <i class="fa-solid fa-percent fa-fw"></i>
-                    </span>
-
-                        </div>
-                    </td>
-                    <td class="pe-0 text-end"><button class="btn btn-success" :disabled="!addAccount || (total + addWeight * 100) > 10000" @click="appendBen()"><i class="fa-solid fa-square-plus fa-fw"></i></button></td>
-                </tr>
-            </tbody>
-        </table>
-        </div>
-    </div>
-    `,
+    template: `<div>
+<p>Current Beneficiaries: ({{bennies?.length}}/8) {{total/100}}%</p><button v-if="!dlux" class="btn btn-sm btn-warning" @click="addBenny('dlux-io', 1000)">Include in DLUX Ecosystem</button>
+<div class="table-responsive">
+<table class="table align-middle">
+<thead>
+<tr>
+<th class="w-50">Username</th>
+<th class="text-center">Reward</th>
+<th></th>
+</tr>
+</thead>
+<tbody class="table-group-divider">
+<tr v-for="ben in bennies">
+<td class="ps-0 w-50">@{{ben.account}}</td>
+<td class="text-center">
+<span class="pe-05"><button type="button" class="btn btn-sm btn-secondary" @click="decBen(ben)">-</button></span>
+<span>{{ben.weight/100}}%</span>
+<span class="ps-05"><button type="button" class="btn btn-sm btn-secondary" @click="incBen(ben)">+</button></span>
+</td>
+<td class="pe-0 text-end"><button class="btn btn-danger" @click="subBenny(ben.account)"><i class="fa-solid fa-trash-can fa-fw"></i></button></td>
+</tr>
+<tr v-if="bennies.length < 8 && total < 10000" style="border-bottom-style: hidden !important;">
+<td class="ps-0 w-50">
+<div class="position-relative">
+<span class="position-absolute start-0 top-50 translate-middle-y ps-2">
+<i class="fa-solid fa-at fa-fw"></i>
+</span>
+<input type="text" placeholder="username" class="ps-4 pe-5 text-info form-control" v-model="addAccount">
+<button class="position-absolute end-0 top-50 translate-middle-y btn btn-outline-secondary border-0 dropdown-toggle square rounded-end" :disabled="!favorites.length" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-star me-1 fa-fw"></i></button>
+<ul class="dropdown-menu bg-black dropdown-menu-dark">
+<li v-for="acc in favorites" class="d-flex align-items-center justify-content-between">
+<a class="dropdown-item rounded-end" role="button" @click="addAccount = acc">@{{acc}}</a>
+<a @click="removeFavorite(acc)" class="mx-1 btn btn-sm btn-secondary" role="button">
+<i class="fa-solid fa-xmark fa-fw"></i>
+</a>
+</li>
+</ul>
+</div>
+</td>
+<td class="text-center">
+<div class="position-relative">
+<input type="number" step="0.01" min="0.01" :max="100 - (total/100)" placeholder="amount" class="pe-4 form-control text-center" v-model="addWeight">
+<span class="position-absolute end-0 top-50 translate-middle-y pe-2">
+<i class="fa-solid fa-percent fa-fw"></i>
+</span>
+</div>
+</td>
+<td class="pe-0 text-end"><button class="btn btn-success" :disabled="!addAccount || (total + addWeight * 100) > 10000" @click="appendBen()"><i class="fa-solid fa-square-plus fa-fw"></i></button></td>
+</tr>
+</tbody>
+</table>
+</div>
+</div> `,
     data() {
         return {
             total: 0,
@@ -116,14 +113,16 @@ export default {
             }
         },
         checkHive(account, amount){
-            let found = -1
+            let found = false
             for (let index = 0; index < this.bennies.length; index++) {
                 if(this.bennies[index].account == account){
-                    this.bennies[index].weight += amount;
-                    found = index;
+                    const old = this.bennies[index].weight
+                    this.bennies[index].weight = amount;
+                    found = true;
+                    if(old != amount)this.finalize()
                 }
             }
-            fetch('https://api.hive.blog', {
+            if(!found)fetch('https://api.hive.blog', {
                 method: 'POST',
                 body: JSON.stringify({  
                     "jsonrpc": "2.0",
@@ -201,7 +200,7 @@ export default {
     watch: {
         'list': {
             handler: function (val, oldVal) {
-                this.list.forEach(benny => {
+                val.forEach(benny => {
                     this.checkHive(benny.account, benny.weight)
                 });
             },
