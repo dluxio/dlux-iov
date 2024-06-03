4f291cae1581c5e91a2ef335a4272b6c0972f7bc
disregardfiat <sjettingerjr@gmail.com>
2024-06-03 19:27:53 -0400

    ++

--- js/contractvue.js
+++ js/contractvue.js
@@ -464,17 +464,17 @@ export default {
                                                                                 <div class="d-flex mb-1">
                                                                                     <div class="me-1">
                                                                                         <div class="position-relative has-validation">
-                                                                                            <input autocapitalize="off" :value="newMeta[contract.i][index * 3 + 1]" placeholder="File Name" pattern="[a-zA-Z0-9_\-]{3,25}"class="form-control form-control-sm bg-dark border-dark text-info">
+                                                                                            <input autocapitalize="off" :value="newMeta[contract.i].meta[index * 3 + 1]" placeholder="File Name" pattern="[a-zA-Z0-9_\-]{3,25}"class="form-control form-control-sm bg-dark border-dark text-info">
                                                                                         </div>
                                                                                     </div>
                                                                                     <div class="mx-1">
                                                                                         <div class="position-relative has-validation">
-                                                                                            <input autocapitalize="off" :value="newMeta[contract.i][index * 3 + 2]" placeholder="File Type" pattern="[a-zA-Z0-9]{1,4}" class="form-control form-control-sm bg-dark border-dark text-info">
+                                                                                            <input autocapitalize="off" :value="newMeta[contract.i].meta[index * 3 + 2]" placeholder="File Type" pattern="[a-zA-Z0-9]{1,4}" class="form-control form-control-sm bg-dark border-dark text-info">
                                                                                         </div>
                                                                                     </div>
                                                                                     <div class="ms-1">
                                                                                         <div class="position-relative has-validation">
-                                                                                            <input autocapitalize="off" :value="newMeta[contract.i][index * 3 + 3]" placeholder="Thumbnail" pattern="https:\/\/[a-z0-9.-\/]+|Qm[a-zA-Z0-9]+" class="form-control form-control-sm bg-dark border-dark text-info">
+                                                                                            <input autocapitalize="off" :value="newMeta[contract.i].meta[index * 3 + 3]" placeholder="Thumbnail" pattern="https:\/\/[a-z0-9.-\/]+|Qm[a-zA-Z0-9]+" class="form-control form-control-sm bg-dark border-dark text-info">
                                                                                         </div>
                                                                                     </div>
                                                                                 </div>
@@ -788,6 +788,7 @@ export default {
                         if(!data.file_contracts[node].m){
                             data.file_contracts[node].m = ""
                             const filesNum = data.file_contracts[node]?.df ? Object.keys(data.file_contracts[node].df).length : 0
+                            console.log(filesNum)
                             this.newMeta[data.file_contracts[node].i] = {
                                 meta: [].fill("", 0, filesNum * 3 + 1),
                             }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2024.06.03.17";
+this.version = "2024.06.03.18";
 
 console.log( "SW:" + this.version + " - online.");
 
