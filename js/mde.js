724f9c465beea2ffd47feaf459b6a58437cf11ce
disregardfiat <sjettingerjr@gmail.com>
2024-06-04 18:19:45 -0400

    ++

--- js/mde.js
+++ js/mde.js
@@ -2,12 +2,30 @@ export default {
     name: "MDE",
     template: `<div><textarea ref="mde"/></div>`,
     emits: ["data"],
+    props: {
+      value: {
+        type: String,
+        default: ""
+      }
+    },
+    methods: {
+      setValue(value) {
+        this.mde.value(value);
+      }
+    },
+    watch: {
+      value(value) {
+        this.setValue(value);
+      }
+    },
     mounted() {
+
       this.mde = new SimpleMDE({ 
         element: this.$refs.mde,
         dragDrop: false });
       this.mde.codemirror.on("change", () => {
         this.$emit("data", this.mde.value())
       });
+      this.mde.value(this.value)
     }
   };
