2819484f18c87f23add9e8cb1a45a672d4fa67df
disregardfiat <sjettingerjr@gmail.com>
2024-06-12 13:44:47 -0400

    ++

--- js/contractvue.js
+++ js/contractvue.js
@@ -438,7 +438,7 @@ export default {
                                                                         <div class="card card-body align-items-center mx-lg-5 my-3">
                                                                             <div class="d-flex flex-column flex-grow-1 mx-1">
                                                                                 <div class="fs-3 fw-lighter">Sharing:</div>
-                                                                                <p v-if="contract.t == spkapi.name">{{pluralFiles(contract.i) ? 'This file is' : 'These files are'}} encrypted. You can add and remove accounts that can decrypt them.</p>
+                                                                                <p v-if="contract.t == spkapi.name">{{pluralFiles(contract.i) ? 'This file is' : 'These files are'}} encrypted. You can add and remove accounts that can decrypt {{pluralFiles(contract.i) ? 'them' : 'it'}}.</p>
                                                                                 <p v-if="contract.t != spkapi.name">{{pluralFiles(contract.i) ? 'This file is' : 'These files are'}} encrypted and shared with the following accounts.</p>
                                                                                 <div class="d-flex mb-2" v-if="contract.t == spkapi.name && contract.encryption.key">
                                                                                     <div class="me-1 flex-grow-1">
@@ -1005,7 +1005,7 @@ export default {
             } index
         },
         pluralFiles(id){
-            return this.newMeta[id].length < 6
+            return this.newMeta[id].length > 5
         },
         getSapi(user = this.account) {
             if (user) fetch(this.sapi + "/@" + user)
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2024.06.12.27";
+this.version = "2024.06.12.28";
 
 console.log( "SW:" + this.version + " - online.");
 
