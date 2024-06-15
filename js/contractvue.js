03133242be9df6893f904b4465dbd2c1355180ae
Mark <m.e.giles@gmail.com>
2024-06-15 08:07:10 -0700

    Update contractvue.js

--- js/contractvue.js
+++ js/contractvue.js
@@ -395,12 +395,17 @@ export default {
                                                                                                     <span class="small ms-1">({{size > 1 ? size/1000000 : size/1000000}} MB)</span>
                                                                                                 </div>
                                                                                                 <div v-if="flagDecode(contract.m).enc && !contract.encryption.key" v-for="(a,b,c) in contract.encryption.accounts">
-                                                                                                    <div v-if="b == spkapi.name" class="rounded bg-white text-black filter-bubble me-1 mb-1 d-flex align-items-center" > <!-- warning class for unencrypted keys --> 
-                                                                                                        <div class="d-flex align-items-center">
-                                                                                                            <i class="fa-solid fa-key fa-fw me-1" :class="{'text-success': contract.encryption.accounts[b].enc_key, 'text-warning': !contract.encryption.accounts[b].enc_key}"></i>
-                                                                                                            <span>{{b}}</span>
-                                                                                                            
-                                                                                                            <div><button type="button" class="ms-2 small btn-white" @click="decryptKey(contract.i)"><i class="fa-solid fa-fw mx-1 fa-lock-open" aria-hidden="true"></i></button></div>
+                                                                                                    <div v-if="b == spkapi.name" class="d-flex flex-row flex-wrap mt-2"> <!-- warning class for unencrypted keys --> 
+                                                                                                        <div class="rounded bg-white text-black filter-bubble me-1 mb-1 d-flex align-items-center">
+                                                                                                            <div class="d-flex align-items-center">
+                                                                                                                <i class="fa-solid fa-key fa-fw me-1" :class="{'text-success': contract.encryption.accounts[b].enc_key, 'text-warning': !contract.encryption.accounts[b].enc_key}"></i>
+                                                                                                                <span>{{b}}</span>
+                                                                                                                <div>
+                                                                                                                    <button type="button" class="ms-2 small btn-white" @click="decryptKey(contract.i)">
+                                                                                                                        <i class="fa-solid fa-fw mx-1 fa-lock-open" aria-hidden="true"></i>
+                                                                                                                    </button>
+                                                                                                                </div>
+                                                                                                            </div>
                                                                                                         </div>
                                                                                                     </div>
                                                                                                 </div>
