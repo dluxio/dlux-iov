0b8c4b9d64f9029ced4954a5783a45a5ad1798a4
Mark <m.e.giles@gmail.com>
2024-06-15 20:34:08 -0700

    Update contractvue.js

--- js/contractvue.js
+++ js/contractvue.js
@@ -467,9 +467,9 @@ export default {
                                                                                                 </div>
 
                                                                                                  <!-- encrypt / save button -->
-                                                                                                <div class="d-flex text-center my-2">
-                                                                                                    <button v-if="unkeyed(contract.i)" class="mx-auto btn btn-lg btn-outline-warning" type="button" @click="checkHive(contract.i)"><i class="fa-solid fa-fw fa-user-lock me-2"></i>Encrypt Keys</button>
-                                                                                                    <button v-if="metaMismatch(contract.i) && !unkeyed(contract.i)" class="btn btn-lg btn-outline-warning mx-auto" type="button" @click="update_meta(contract.i)"><i class="fa-solid fa-floppy-disk fa-fw me-2"></i>Save Changes</button>
+                                                                                                <div class="d-flex text-center">
+                                                                                                    <button v-if="unkeyed(contract.i)" class="mx-auto my-2 btn btn-lg btn-outline-warning" type="button" @click="checkHive(contract.i)"><i class="fa-solid fa-fw fa-user-lock me-2"></i>Encrypt Keys</button>
+                                                                                                    <button v-if="metaMismatch(contract.i) && !unkeyed(contract.i)" class="btn btn-lg btn-outline-warning mx-auto my-2" type="button" @click="update_meta(contract.i)"><i class="fa-solid fa-floppy-disk fa-fw me-2"></i>Save Changes</button>
                                                                                                 </div>
 
                                                                                                 
@@ -479,8 +479,8 @@ export default {
                                                                                     </div>
 
                                                                                      <!-- save button -->
-                                                                                    <div class="d-flex text-center my-2">
-                                                                                        <button v-if="metaMismatch(contract.i) && !flagDecode(contract.m).enc" class="btn btn-lg btn-outline-warning mx-auto" type="button" @click="update_meta(contract.i)"><i class="fa-solid fa-floppy-disk fa-fw me-2"></i>Save Changes</button>
+                                                                                    <div class="d-flex text-center">
+                                                                                        <button v-if="metaMismatch(contract.i) && !flagDecode(contract.m).enc" class="btn btn-lg btn-outline-warning mx-auto my-2" type="button" @click="update_meta(contract.i)"><i class="fa-solid fa-floppy-disk fa-fw me-2"></i>Save Changes</button>
                                                                                     </div>
                                                                             </div>
                                                                         </div>
