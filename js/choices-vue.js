a4a9987f11f93ee59b4728a3c2650e15fc4a22ac
disregardfiat <sjettingerjr@gmail.com>
2024-06-17 19:57:03 -0400

    ++

--- js/choices-vue.js
+++ js/choices-vue.js
@@ -64,12 +64,6 @@ export default {
       msg: "",
       Choices: null,
       tags: [{
-        value: 'encrypted',
-        label: 'Encrypted',
-        selected: false,
-        disabled: false,
-      },
-      {
         value: 'autorenew',
         label: 'Auto-Renew',
         selected: false,
@@ -81,116 +75,371 @@ export default {
         selected: false,
         disabled: false,
         customProperties: {
-          description: 'Not Safe For Work',
-          random: 'Another random custom property'
+          description: 'Not Safe For Work'
         },
-      }],
+      }, {
+        value: 'exe',
+        label: 'Executable',
+        selected: false,
+        disabled: false,
+        customProperties: {
+          description: 'Is an executable file'
+        },
+      }
+      ],
       labels: [{
-          value: 'red',
-          label: 'Red',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'orange',
-          label: 'Orange',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'yellow',
-          label: 'Yellow',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'green',
-          label: 'Green',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'blue',
-          label: 'Blue',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'purple',
-          label: 'Purple',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'grey',
-          label: 'Grey',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'work',
-          label: 'Work',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'personal',
-          label: 'Personal',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'family',
-          label: 'Family',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'friends',
-          label: 'Friends',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'projects',
-          label: 'Projects',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'finance',
-          label: 'Finance',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'health',
-          label: 'Health',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'education',
-          label: 'Education',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'travel',
-          label: 'Travel',
-          selected: false,
-          disabled: false,
-        },
-        {
-          value: 'events',
-          label: 'Events',
-          selected: false,
-          disabled: false,
-        }],
+        value: 0,
+        label: 'Important',
+        selected: false,
+        disabled: false,
+      },{
+        value: 1,
+        label: 'Red',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 2,
+        label: 'Orange',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 3,
+        label: 'Yellow',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 4,
+        label: 'Green',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 5,
+        label: 'Blue',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 6,
+        label: 'Purple',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 7,
+        label: 'Grey',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 8,
+        label: 'Work',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 9,
+        label: 'Personal',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 10,
+        label: 'Family',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 11,
+        label: 'Friends',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 12,
+        label: 'Projects',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 13,
+        label: 'Finance',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 14,
+        label: 'Health',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 15,
+        label: 'Education',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 16,
+        label: 'Travel',
+        selected: false,
+        disabled: false,
+      },
+      {
+        value: 17,
+        label: 'Events',
+        selected: false,
+        disabled: false,
+      },{
+        value: 18,
+        label: 'Photography',
+        selected: false,
+        disabled: false,
+      },{
+        value: 19,
+        label: 'Gaming',
+        selected: false,
+        disabled: false,
+      },{
+        value: 20,
+        label: 'Sports',
+        selected: false,
+        disabled: false,
+      },{
+        value: 21,
+        label: 'Blogging',
+        selected: false,
+        disabled: false,
+      },{
+        value: 22,
+        label: 'Journalism',
+        selected: false,
+        disabled: false,
+      },{
+        value: 21,
+        label: 'Research',
+        selected: false,
+        disabled: false,
+      },{
+        value: 22,
+        label: 'Meme',
+        selected: false,
+        disabled: false,
+      },{
+        value: 23,
+        label: 'Music',
+        selected: false,
+        disabled: false,
+      },{
+        value: 24,
+        label: 'Media',
+        selected: false,
+        disabled: false,
+      },{
+        value: 25,
+        label: 'News',
+        selected: false,
+        disabled: false,
+      },{
+        value: 26,
+        label: 'Archive',
+        selected: false,
+        disabled: false,
+      },{
+        value: 27,
+        label: 'Fashion',
+        selected: false,
+        disabled: false,
+      },{
+        value: 28,
+        label: 'Food',
+        selected: false,
+        disabled: false,
+      },{
+        value: 29,
+        label: 'Cooking',
+        selected: false,
+        disabled: false,
+      },{
+        value: 30,
+        label: 'DIY',
+        selected: false,
+        disabled: false,
+      },{
+        value: 31,
+        label: 'Art',
+        selected: false,
+        disabled: false,
+      },{
+        value: 32,
+        label: 'Design',
+        selected: false,
+        disabled: false,
+      },{
+        value: 33,
+        label: 'Technology',
+        selected: false,
+        disabled: false,
+      },{
+        value: 34,
+        label: 'Religion',
+        selected: false,
+        disabled: false,
+      },{
+        value: 35,
+        label: 'Government',
+        selected: false,
+        disabled: false,
+      },{
+        value: 36,
+        label: 'Politics',
+        selected: false,
+        disabled: false,
+      },{
+        value: 37,
+        label: 'Science',
+        selected: false,
+        disabled: false,
+      },
+      // {
+      //   value: 38,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 39,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 40,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 41,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 42,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 43,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 44,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 45,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 46,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 47,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 48,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 49,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 50,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 51,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 52,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 53,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 54,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 55,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 56,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 57,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 58,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 59,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 60,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 61,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 62,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // },{
+      //   value: 63,
+      //   label: '',
+      //   selected: false,
+      //   disabled: false,
+      // }
+    ],
     }
   },
   methods: {
-    passData(d){
+    passData(d) {
       this.$emit("data", template)
     },
     setUp() {
@@ -205,8 +454,8 @@ export default {
   },
   watch: {
     'Choices': {
-      handler: function (o,n) {
-        console.log(o,n)
+      handler: function (o, n) {
+        console.log(o, n)
       },
       deep: true
     },
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2024.06.17.18";
+this.version = "2024.06.17.19";
 
 console.log( "SW:" + this.version + " - online.");
 
