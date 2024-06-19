00c4330618d0d7f0c1fccd01e63ce1dd64c3b2e2
disregardfiat <sjettingerjr@gmail.com>
2024-06-18 22:28:52 -0400

    ++

--- js/mde.js
+++ js/mde.js
@@ -1,37 +1,38 @@
 export default {
-    name: "MDE",
-    template: `<div><textarea ref="mde"/></div>`,
-    emits: ["data"],
-    props: {
-      prop_insert: {
-        type: String,
-        default: ""
-      }
-    },
-    methods: {
-      insertText(value) {
-        console.log("insertText", value)
-        var pos = this.mde.codemirror.getCursor();
-        this.mde.codemirror.setSelection(pos, pos);
-        this.mde.codemirror.replaceSelection(value);
-
-      }
-    },
-    watch: {
-      'prop_insert'(newValue){
-        if(newValue){
-            console.log(2, {newValue})
-            this.insertText(newValue)
-        }
+  name: "MDE",
+  template: `<div><textarea ref="mde"/></div>`,
+  emits: ["data"],
+  props: {
+    insert: {
+      type: String,
+      default: ""
     }
-    },
-    mounted() {
+  },
+  methods: {
+    insertText(value) {
+      console.log("insertText", value)
+      var pos = this.mde.codemirror.getCursor();
+      this.mde.codemirror.setSelection(pos, pos);
+      this.mde.codemirror.replaceSelection(value);
 
-      this.mde = new SimpleMDE({ 
-        element: this.$refs.mde,
-        dragDrop: false });
-      this.mde.codemirror.on("change", () => {
-        this.$emit("data", this.mde.value())
-      });
     }
-  };
+  },
+  watch: {
+    'insert'(newValue) {
+      if (newValue) {
+        console.log(2, { newValue })
+        this.insertText(newValue)
+      }
+    }
+  },
+  mounted() {
+
+    this.mde = new SimpleMDE({
+      element: this.$refs.mde,
+      dragDrop: false
+    });
+    this.mde.codemirror.on("change", () => {
+      this.$emit("data", this.mde.value())
+    });
+  }
+};
