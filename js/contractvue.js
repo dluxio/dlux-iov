f5a28f5a344b40501c206f708db5e426ec17c5a8
disregardfiat <sjettingerjr@gmail.com>
2024-06-29 14:55:52 -0400

    ++

--- js/contractvue.js
+++ js/contractvue.js
@@ -1189,8 +1189,7 @@ export default {
                             } else {
                                 if (data.file_contracts[node].m.indexOf('"') >= 0) data.file_contracts[node].m = JSON.parse(data.file_contracts[node].m)
                                 var encData = data.file_contracts[node].m.split(',')[0] || ''
-                                var renew = false = this.Base64toNumber(encData[0] || '0') & 1
-                                var renew = false
+                                var renew  = this.Base64toNumber(encData[0] || '0') & 1
                                 var encAccounts = []
                                 var encrypted = false
                                 if(encData){
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2024.06.29.5";
+this.version = "2024.06.29.6";
 
 console.log( "SW:" + this.version + " - online.");
 
