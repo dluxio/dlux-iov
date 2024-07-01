505ef2f04c9cc145afc82a8fd895f51a16b459a4
disregardfiat <sjettingerjr@gmail.com>
2024-07-01 11:41:17 -0400

    ++

--- js/contractvue.js
+++ js/contractvue.js
@@ -1050,13 +1050,13 @@ export default {
         update_meta(contract) {
             return new Promise((resolve, reject) => {
                 console.log(this.newMeta[contract], contract)
-                var enc_string = `${this.newMeta[contract].contract.autoRenew ? 1 : 0}`
+                var enc_string = ''
                 for (var acc in this.contractIDs[contract].encryption.accounts) {
                     if (this.contractIDs[contract].encryption.accounts[acc].enc_key) enc_string += `${this.contractIDs[contract].encryption.accounts[acc].enc_key}@${acc};`
                 }
                 //remove last ;
                 enc_string = enc_string.slice(0, -1)
-                this.newMeta[contract].contract.enc_string = enc_string
+                this.newMeta[contract].contract.enc_string = `${this.newMeta[contract].contract.autoRenew ? '1' : '0' }${enc_string}`
                 var cids = Object.keys(this.newMeta[contract])
                 cids = cids.sort((a, b) => {
                     if (a > b) return 1
