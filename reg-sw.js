f3583b5d0bec79b666553c3a13e4a7eca27fe536
Mark <m.e.giles@gmail.com>
2025-03-12 17:58:12 -0700

    test

--- reg-sw.js
+++ reg-sw.js
@@ -1,7 +1,7 @@
 var activeWorker = 0;
 const enableServiceWorker = true;
 if ('serviceWorker' in navigator && enableServiceWorker) {
-    const version = '2025.03.12.28'; // Increment for testing
+    const version = '2025.03.12.29'; // Increment for testing
     console.log('Registering service worker with version:', version);
 
     // Clean up old registrations
