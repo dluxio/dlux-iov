522ae540c3195989174a5791602f127e350d039b
Mark <m.e.giles@gmail.com>
2025-04-09 18:11:36 -0700

    pri

--- reg-sw.js
+++ reg-sw.js
@@ -1,5 +1,5 @@
 if (window.location.hostname.includes('dlux') && 'serviceWorker' in navigator) {
-    const version = '2025.04.09.27';
+    const version = '2025.04.09.28';
     console.log('Registering service worker with version:', version);
 
     navigator.serviceWorker.getRegistration('/')
