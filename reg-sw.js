fd0efc3cf31b8848936936bb74c84ad9aa13576e
disregardfiat <sjettingerjr@gmail.com>
2025-04-11 03:38:31 -0300

    ++

--- reg-sw.js
+++ reg-sw.js
@@ -1,5 +1,5 @@
 if (window.location.hostname.includes('dlux') && 'serviceWorker' in navigator) {
-    const version = '2025.04.11.7';
+    const version = '2025.04.11.8';
     console.log('Registering service worker with version:', version);
 
     navigator.serviceWorker.getRegistration('/')
