bc705001714e08421a33717bd5fd591d537387d0
disregardfiat <sjettingerjr@gmail.com>
2025-04-12 17:26:45 -0300

    ++

--- reg-sw.js
+++ reg-sw.js
@@ -1,6 +1,6 @@
 var activeWorker = null
 if (window.location.hostname.includes('dlux') && 'serviceWorker' in navigator) {
-    const version = '2025.04.12.10';
+    const version = '2025.04.12.11';
     console.log('Registering service worker with version:', version);
 
     navigator.serviceWorker.getRegistration('/')
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2025.04.12.10";
+this.version = "2025.04.12.11";
 console.log("SW:" + version + " - online.");
 const CACHE_NAME = "sw-cache-v" + version;
 
