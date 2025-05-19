e07a022285726c452e9bce145a5e816a9c5ac9a1
Mark <m.e.giles@gmail.com>
2025-05-19 08:27:48 -0700

    ++

--- reg-sw.js
+++ reg-sw.js
@@ -1,6 +1,6 @@
 var activeWorker = null
 if (window.location.hostname.includes('dlux') && 'serviceWorker' in navigator) {
-    const version = '2025.05.19.1';
+    const version = '2025.05.19.2';
     console.log('Registering service worker with version:', version);
 
     navigator.serviceWorker.getRegistration('/')
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2025.05.19.1";
+this.version = "2025.05.19.2";
 console.log("SW:" + version + " - online.");
 const CACHE_NAME = "sw-cache-v" + version;
 
