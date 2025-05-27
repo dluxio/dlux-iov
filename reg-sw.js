640f1a4a3d6579851bb4fd43a7a0460302657908
disregardfiat <sjettingerjr@gmail.com>
2025-05-27 12:21:03 -0300

    Deploy 2025.05.27.4

--- reg-sw.js
+++ reg-sw.js
@@ -1,6 +1,6 @@
 var activeWorker = null;
 if (window.location.hostname.includes('dlux') && 'serviceWorker' in navigator) {
-    const version = '2025.05.27.3'; // Ensure this version matches sw.js
+    const version = '2025.05.27.4'; // Ensure this version matches sw.js
     console.log('Desired service worker version:', version);
 
     navigator.serviceWorker.getRegistration('/')
