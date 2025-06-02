2ff35be08aae959d1228bac5f1f233105be4a442
Mark <m.e.giles@gmail.com>
2025-06-02 11:45:23 -0700

    2025.06.02.7

--- js/sw-monitor.js
+++ js/sw-monitor.js
@@ -4,7 +4,7 @@ export default {
       // Service Worker states
       swStatus: 'loading', // loading, current, update-available, installing, updated, error
       swVersion: null,
-      desiredVersion: '2025.06.02.6', // Should match sw.js version
+      desiredVersion: '2025.06.02.7', // Should match sw.js version
 
       // PWA Install states
       installStatus: 'unknown', // unknown, available, installed, not-supported
