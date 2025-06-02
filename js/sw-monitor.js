6d6c5f1c67bae1874734a76c5ad55943b9c08561
Mark <m.e.giles@gmail.com>
2025-06-01 21:48:36 -0700

    2025.06.01.22

--- js/sw-monitor.js
+++ js/sw-monitor.js
@@ -4,7 +4,7 @@ export default {
       // Service Worker states
       swStatus: 'loading', // loading, current, update-available, installing, updated, error
       swVersion: null,
-      desiredVersion: '2025.06.01.21', // Should match sw.js version
+      desiredVersion: '2025.06.01.22', // Should match sw.js version
 
       // PWA Install states
       installStatus: 'unknown', // unknown, available, installed, not-supported
@@ -590,7 +590,7 @@ export default {
                   <i class="fa-solid fa-sync fa-spin fa-fw me-1" v-else-if="swStatus === 'installing'"></i> 
                   <i class="fa-solid fa-exclamation-triangle fa-fw me-1" v-else-if="swStatus === 'error'"></i> 
                   <i class="fa-solid fa-download fa-fw me-1" v-else-if="installStatus === 'available'"></i>
-                  <span>Alerts</span>
+                  <span>App</span>
                   <span class="d-none" v-if="showUpdateNotification || installStatus === 'available'">!</span> 
               </button>
                </div>
@@ -647,7 +647,7 @@ export default {
               Install DLUX as an app for better performance and offline access.
             </p>
             <button @click="installPWA" class="btn btn-outline-primary btn-sm w-100">
-              <i class="fa-solid fa-mobile-screen me-1"></i>
+              <i class="fa-solid fa-download me-1"></i>
               {{ installButtonText }}
             </button>
           </div>
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-this.version = "2025.06.01.21";
+this.version = "2025.06.01.22";
 console.log("SW:" + version + " - online.");
 const CACHE_NAME = "sw-cache-v" + version;
 
